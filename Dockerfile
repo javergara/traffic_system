2e534c12110f2645695b1ef9917fabfcedea84ff
Javier Vergara <javier.a.vergara.zorrilla@pwc.com>
2023-03-22 02:48:25 -0500

    all running

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,59 @@
-# Base image
 FROM python:3.11-slim-buster
 
+
+
 # Install dependencies
-RUN apt-get update && apt-get install -y nodejs npm
+
+RUN apt-get update && apt-get install -y curl && \
+
+    curl -fsSL https://deb.nodesource.com/setup_16.x | bash - && \
+
+    apt-get install -y nodejs
+
+
 
 # Set the working directory for the app
+
 WORKDIR /app
+
+
 
 # Install Python dependencies
+
 COPY requirements.txt .
+
 RUN pip install -r requirements.txt
 
+
+
 # Copy the backend code into the container
+
 COPY main.py .
+
 COPY utils/ ./utils/
+
 COPY db/ ./db/
 
+
+
 # Copy the frontend code into the container
+
 COPY client/ ./client/
 
+
+
 # Build the frontend
+
 WORKDIR /app/client
-RUN npm install && npm run build
+
+
 
 # Set the working directory back to the app root
+
 WORKDIR /app
 
+
+
 # Start the server
+
 CMD ["sh", "-c", "uvicorn main:app --host 0.0.0.0 --port 8000 & node client/server.js"]
--- client/server.js
+++ client/server.js
@@ -1,41 +1,87 @@
 const http = require('http');
+
 const fs = require('fs');
 
+const path = require('path');
+
+
+
 const server = http.createServer((req, res) => {
+
   if (req.url === '/') {
+
     // Serve the index.html file
-    fs.readFile('./index.html', (err, data) => {
+
+    const filePath = path.join(__dirname, 'index.html'); // create an absolute path
+
+    fs.readFile(filePath, (err, data) => {
+
       if (err) {
+
         res.writeHead(500, {'Content-Type': 'text/plain'});
+
         res.write('Error loading index.html');
+
         res.end();
+
         return;
+
       }
+
       res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+
       res.write(data);
+
       res.end();
+
     });
+
   } else if (req.url === '/index.js') {
+
     // Serve the index.js file
-    fs.readFile('./index.js', (err, data) => {
+
+    const filePath = path.join(__dirname, 'index.js'); // create an absolute path
+
+    fs.readFile(filePath, (err, data) => {
+
       if (err) {
+
         res.writeHead(500, {'Content-Type': 'text/plain'});
+
         res.write('Error loading index.js');
+
         res.end();
+
         return;
+
       }
+
       res.writeHead(200, {'Content-Type': 'application/javascript; charset=utf-8'});
+
       res.write(data);
+
       res.end();
+
     });
+
   } else {
+
     // Return a 404 error for all other requests
+
     res.writeHead(404, {'Content-Type': 'text/plain'});
+
     res.write('Page not found');
+
     res.end();
+
   }
+
 });
 
+
+
 server.listen(3000, () => {
+
   console.log('Server running on port 3000');
+
 });
